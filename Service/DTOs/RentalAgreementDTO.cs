b13cdd38746e8ff3144a4d4e3ad618a1d4032e5c
Bergewing <s.bergewing@hotmail.com>
2022-04-22 14:58:33 +0200

    Added info in rentalDTO, Added info in rentalHTML, fixed rental search path. Tested in swagger, works good.

--- Service/DTOs/RentalAgreementDTO.cs
+++ Service/DTOs/RentalAgreementDTO.cs
@@ -8,5 +8,29 @@ namespace Service.DTOs
 {
     public class RentalAgreementDTO
     {
+        public string? BuyerName { get; set; }
+        public string? BuyerSocialSecurity { get; set; }
+        public string? BuyerAddress { get; set; }
+        public string? BuyerPostalCode { get; set; }
+        public string? BuyerCity { get; set; }
+        public string? BuyerPhone { get; set; }
+        public string? BuyerEmail { get; set; }
+
+        public string? SellerName { get; set; }
+        public string? SellerSocialSecurity { get; set; }
+        public string? SellerAddress { get; set; }
+        public string? SellerPostalCode { get; set; }
+        public string? SellerCity { get; set; }
+        public string? SellerPhone { get; set; }
+        public string? SellerEmail { get; set; }
+
+        public string? ObjectName { get; set; }
+        public string? ObjectNumber { get; set; }
+        public string? ObjectAddress { get; set; }
+        public double? Price { get; set; }
+        public double? Area  { get; set; }
+        public double? AmountOfRooms { get; set; }
+        public string? Purpose { get; set; }
+        public string? Other { get; set; }
     }
 }
