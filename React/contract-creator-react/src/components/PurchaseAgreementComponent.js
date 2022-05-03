7a3d20834698601583108b021c3c7b339b6ba104
Robin Lögdahl <robilogd@gmail.com>
2022-05-03 15:21:50 +0200

    test more

--- React/contract-creator-react/src/components/PurchaseAgreementComponent.js
+++ React/contract-creator-react/src/components/PurchaseAgreementComponent.js
@@ -1,11 +1,102 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class PurchaseAgreementComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+  handleSubmit(event) {
+    alert("A name was submitted: " + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div>PurchaseAgreementComponent</div>
-    )
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          För- och efternamn
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Personnummer
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Adress
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Telefon
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Adress
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
   }
 }
 
-export default PurchaseAgreementComponent
+export default PurchaseAgreementComponent;
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+  handleSubmit(event) {
+    alert("A name was submitted: " + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {" "}
+        <label>
+          Name:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />{" "}
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
