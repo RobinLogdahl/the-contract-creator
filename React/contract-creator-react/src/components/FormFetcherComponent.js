6e3b1f43cec44a94c3feaa6528a26da8b1514b43
Bergewing <s.bergewing@hotmail.com>
2022-05-09 11:23:32 +0200

    Fick If satsen att funka, loggade värderna från propsen skickade iväg props från agreementselector, skapade rental agreement

--- React/contract-creator-react/src/components/FormFetcherComponent.js
+++ React/contract-creator-react/src/components/FormFetcherComponent.js
@@ -5,21 +5,45 @@ import SaveToPDFComponent from "./SaveToPDFComponent";
 
 const FormFetcherComponent = (props) => {
 
-    const handleIncomingData = () => {
-        let arr = Object.values(props);
+  let arr = Object.values(props);
         console.log(arr);
-        
-    }
-
-    handleIncomingData();
+        debugger
+        const [value1, value2, value3] = arr;
+        console.log(value1)
+        console.log(value2)
+        console.log(value3)
 
     
-  return (
-    <Fragment>
-        <SaveToPDFComponent />   
-        <PurchaseAgreementComponent />
-    </Fragment>
-  );
-};
+
+    
+  return(
+    <div>
+    {(() =>{
+                if(value1 === 'Köpeskontrakt'){
+                  return(
+                      <Fragment>
+                      <SaveToPDFComponent />   
+                      <PurchaseAgreementComponent />
+                      </Fragment>
+                  )
+                  
+                }
+                else if(value1 === 'Hyreskontrakt'){
+                  return <h1>Hyreskontrakt</h1>
+                }
+                
+    })()}
+    </div>
+  )
+}
+
+      
+    
+  // }
+  //   <Fragment>
+  //       <SaveToPDFComponent />   
+  //       <PurchaseAgreementComponent />
+  //   </Fragment>
+  // );
 
 export default FormFetcherComponent;
---
+++ React/contract-creator-react/src/components/RentalAgreementComponent.js
@@ -0,0 +1,220 @@
+import React from "react";
+import { useState } from "react";
+import './PurchaseAgreementComponent.css';
+import FormComponent from "./FormComponent";
+
+function PurchaseAgreementComponent() {
+  const [buyerName, setBuyerName] = useState("");
+  const [buyerSocialSecurity, setBuyerSocial] = useState("");
+  const [buyerAddress, setBuyerAddress] = useState("");
+  const [buyerPostalCode, setBuyerPostalCode] = useState("");
+  const [buyerCity, setBuyerCity] = useState("");
+  const [buyerPhone, setBuyerPhone] = useState("");
+  const [buyerEmail, setBuyerEmail] = useState("");
+
+  const [sellerName, setSellerName] = useState("");
+  const [sellerSocialSecurity, setSellerSocial] = useState("");
+  const [sellerAddress, setSellerAddress] = useState("");
+  const [sellerPostalCode, setSellerPostalCode] = useState("");
+  const [sellerCity, setSellerCity] = useState("");
+  const [sellerPhone, setSellerPhone] = useState("");
+  const [sellerEmail, setSellerEmail] = useState("");
+
+  const [objectName, setObjectName] = useState("");
+  const [objectNumber, setObjectNumber] = useState("");
+  const [objectAdress, setObjectAdress] = useState("");
+  const [objectPrice, setObjectPrice] = useState("");
+  const [objectArea, setObjectArea] = useState("");
+  const [objectAmountOfRooms, setObjectAmountOfRooms] = useState("");
+  const [objectPurpose, setObjectPurpose] = useState("");
+  const [other, setOther] = useState("");
+
+  const handleButtonClicked = (event) => {
+    event.preventDefault();
+      fetch("https://localhost:7029/Contract/purchase-agreement", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          buyerName: buyerName,
+          buyerSocialSecurity: buyerSocialSecurity,
+          buyerAddress: buyerAddress,
+          buyerPostalCode: buyerPostalCode,
+          buyerCity: buyerCity,
+          buyerPhone: buyerPhone,
+          buyerEmail: buyerEmail,
+          sellerName: sellerName,
+          sellerSocialSecurity: sellerSocialSecurity,
+          sellerAddress: sellerAddress,
+          sellerPostalCode: sellerPostalCode,
+          sellerCity: sellerCity,
+          sellerPhone: sellerPhone,
+          sellerEmail: sellerEmail,
+          objectName: objectName,
+          objectNumber: objectNumber,
+          objectAdress: objectAdress,
+          price: price,
+          objectArea: objectArea,
+          objectAmountOfRooms: objectAmountOfRooms,
+          objectPurpose: objectPurpose,
+          other: other,
+        }),
+      })
+        .then(function (response) {
+          return response.text();
+        })
+        .then(function (html) {
+          var root = document.getElementById("htmlinsert");
+          root.innerHTML = html;
+        })
+        .catch(function (err) {
+          console.warn("Something went wrong.", err);
+        });
+    };
+  
+
+  return (
+    <div id="htmlinsert">
+      <form id="form">
+        <p>Köparens uppgifter</p>
+        <FormComponent
+          label="För- och efternamn"
+          id="buyerName"
+          value={buyerName}
+          onChange={(e) => setBuyerName(e.target.value)}
+        />
+        <FormComponent
+          label="Personnummer"
+          id="buyerSocialSecurity"
+          value={buyerSocialSecurity}
+          onChange={(e) => setBuyerSocial(e.target.value)}
+        />
+        <FormComponent
+          label="Adress"
+          id="buyerAddress"
+          value={buyerAddress}
+          onChange={(e) => setBuyerAddress(e.target.value)}
+        />
+        <FormComponent
+          label="Postkod"
+          id="buyerPostalCode"
+          value={buyerPostalCode}
+          onChange={(e) => setBuyerPostalCode(e.target.value)}
+        />
+        <FormComponent
+          label="Stad"
+          id="buyerCity"
+          value={buyerCity}
+          onChange={(e) => setBuyerCity(e.target.value)}
+        />
+        <FormComponent
+          label="Telefon"
+          id="buyerPhone"
+          value={buyerPhone}
+          onChange={(e) => setBuyerPhone(e.target.value)}
+        />
+        <FormComponent
+          label="Email"
+          id="buyerEmail"
+          value={buyerEmail}
+          onChange={(e) => setBuyerEmail(e.target.value)}
+        />
+        <p>Säljarens uppgifter</p>
+        <FormComponent
+          label="För- och efternamn"
+          id="socialName"
+          value={sellerName}
+          onChange={(e) => setSellerName(e.target.value)}
+        />
+        <FormComponent
+          label="Personnummer"
+          id="sellerSocialSecurity"
+          value={sellerSocialSecurity}
+          onChange={(e) => setSellerSocial(e.target.value)}
+        />
+        <FormComponent
+          label="Adress"
+          id="sellerAddress"
+          value={sellerAddress}
+          onChange={(e) => setSellerAddress(e.target.value)}
+        />
+        <FormComponent
+          label="Postkod"
+          id="sellerPostalCode"
+          value={sellerPostalCode}
+          onChange={(e) => setSellerPostalCode(e.target.value)}
+        />
+        <FormComponent
+          label="Stad"
+          id="sellerCity"
+          value={sellerCity}
+          onChange={(e) => setSellerCity(e.target.value)}
+        />
+        <FormComponent
+          label="Telefon"
+          id="sellerPhone"
+          value={sellerPhone}
+          onChange={(e) => setSellerPhone(e.target.value)}
+        />
+        <FormComponent
+          label="Email"
+          id="sellerEmail"
+          value={sellerEmail}
+          onChange={(e) => setSellerEmail(e.target.value)}
+        />
+        <p>Produkt</p>
+        <FormComponent
+          label="HyresObjektetets namn"
+          id="objectName"
+          value={objectName}
+          onChange={(e) => setObjectName(e.target.value)}
+        />
+        <FormComponent
+          label="Nummer"
+          id="objectNumber"
+          value={objectNumber}
+          onChange={(e) => setObjectNumber(e.target.value)}
+        />
+        <FormComponent
+          label="Adress"
+          id="objectName"
+          value={objectAdress}
+          onChange={(e) => setObjectAdress(e.target.value)}
+        />
+        <FormComponent
+          label="Pris"
+          id="objectPrice"
+          value={objectPrice}
+          onChange={(e) => setObjectPrice(e.target.value)}
+        />
+        <FormComponent
+          label="Area i kvm"
+          id="objectArea"
+          value={objectArea}
+          onChange={(e) => setObjectArea(e.target.value)}
+        />
+        <FormComponent
+          label="Antal rum"
+          id="objectAmountOfRooms"
+          value={objectAmountOfRooms}
+          onChange={(e) => setObjectAmountOfRooms(e.target.value)}
+        />
+        <FormComponent
+          label="Syfte"
+          id="objectPurpose"
+          value={objectPurpose}
+          onChange={(e) => setObjectPurpose(e.target.value)}
+        />
+        <FormComponent
+          label="Övrig information"
+          id="other"
+          value={other}
+          onChange={(e) => setOther(e.target.value)}
+        />
+
+        <button onClick={handleButtonClicked}>Generera Avtal</button>
+      </form>
+    </div>
+  );
+}
+
+export default PurchaseAgreementComponent;
