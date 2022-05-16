b599e42fe8c205a019cb9b5c41bfc16ba4928d4e
Robin Lögdahl <robilogd@gmail.com>
2022-05-16 10:51:08 +0200

    more push

--- React/contract-creator-react/src/components/PurchaseAgreementComponent.js
+++ React/contract-creator-react/src/components/PurchaseAgreementComponent.js
@@ -3,8 +3,13 @@ import "./PurchaseAgreementComponent.css";
 import useBuyerOneInputComponent from "./BuyerOneInputComponent";
 import useSellerOneInputComponent from "./SellerOneInputComponent";
 import useObjectComponent from "./ObjectComponent";
+import html2canvas from "html2canvas";
+// import PDF, { Text, AddPage, Line, Image, Table, Html } from 'jspdf-react'
+import { jsPDF } from "jspdf";
 
 function PurchaseAgreementComponent() {
+  let clickedButton = 1;
+
   const {
     renderBuyer,
     buyerName,
@@ -21,14 +26,23 @@ function PurchaseAgreementComponent() {
     sellerPhone,
   } = useSellerOneInputComponent();
 
-  const {
-    renderObject,
-    object,
-    price,
-    other, } = useObjectComponent();
+  const { renderObject, object, price, other } = useObjectComponent();
+
+  const GeneratePDF = (event) => {
+    const filename = `Köpeskontrakt_test.pdf`;
+
+    html2canvas(document.querySelector("#contract-container"), {
+      scale: 2,
+    }).then((canvas) => {
+      let pdf = new jsPDF("p", "mm", "a4");
+      pdf.addImage(canvas.toDataURL("image/png"), "PNG", 0, 0, 211, 298);
+      pdf.save(filename);
+    });
+  };
 
   const handleButtonClicked = (event) => {
     event.preventDefault();
+    clickedButton = 2;
     fetch("https://localhost:7029/Contract/purchase-agreement", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -61,21 +75,33 @@ function PurchaseAgreementComponent() {
   return (
     <div id="sdd">
       <div>
-        <button className="primaryButton">Föregående steg</button>
-        <button className="primaryButton" onClick={handleButtonClicked}>
-          Generera Avtal
-        </button>
+        <button className="primaryButton">Börja om</button>
+        {(() => {
+          switch (clickedButton) {
+            case 1:
+              return (
+                <button className="primaryButton" onClick={handleButtonClicked}>
+                  Generera Avtal
+                </button>
+              );
+            case 2:
+              return (
+                <button className="primaryButton" onClick={GeneratePDF}>
+                  Spara som PDF
+                </button>
+              );
+          }
+        })()}
       </div>
       <div id="htmlinsert">
-      <form id="form">
-        <p>Köparens uppgifter</p>
-        {renderBuyer}
-        <p>Säljarens uppgifter</p>
-        {renderSeller}
-        <p>Produkt</p>
-        {renderObject}
-
-      </form>
+        <form id="form">
+          <p>Köparens uppgifter</p>
+          {renderBuyer}
+          <p>Säljarens uppgifter</p>
+          {renderSeller}
+          <p>Produkt</p>
+          {renderObject}
+        </form>
       </div>
     </div>
   );
